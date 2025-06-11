3afaeca1295e36e3d0c1620eb679e886fb9c0e8e
Axitk27 <axit.kakadiya@outlook.com>
2025-06-11 20:24:33 +0200

    Pandas Case Study with Streamlit

--- Project/Pandas Case Study - Indian Startup Funding/main.py
+++ Project/Pandas Case Study - Indian Startup Funding/main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-
-df = pd.read_csv('Resources\startup_funding.csv')
+# import matplotlib.pyplot as plt
 
 st.sidebar.title('Startup Funding Analysis')
 option = st.sidebar.selectbox('Select one',['Overall Analysis','StartUp','Investor'])
@@ -9,8 +8,8 @@ option = st.sidebar.selectbox('Select one',['Overall Analysis','StartUp','Invest
 
 ####################################################
 #finding the unique name in the company data       
-df = pd.read_csv('Resources\startup_funding.csv')
-company_data = sorted(df['Startup Name'].unique())
+df = pd.read_csv('Resources\startup_cleaned.csv')
+company_data = sorted(df['Startup'].unique())
 # L = []
 # # for item in data:
 # #     L.append(item.strip().replace('"',''))
@@ -24,11 +23,33 @@ company_data = sorted(df['Startup Name'].unique())
 ###################################################
 #Investor data
 
-df['Investors Name'] = df['Investors Name'].fillna('Undisclosed')
-investor_data = sorted(df['Investors Name'].unique())
+investor_data = sorted(set(df['Investors'].str.split(',').sum()))
+investor_data.remove('')
+invesror_data = df['Investors'].str.strip(" ")
 
 ###################################################
 
+
+# Function for investor
+####################################################
+def load_investor_details(investor):
+    st.title(selected_investor)
+   
+    #load the top 5 data of the investor 
+    last5_df = df[df['Investors'].str.contains('investor')].head(5)[['Date','Startup','Vertical','City','Round','Amount']]
+    st.subheader('Most Recent Investment')
+    st.dataframe(last5_df)
+
+        #biggest investment
+    col1,col2 = st.columns(2)
+    with col1:   
+        big_series = df[df['Investors'].str.contains(investor)].groupby('Startup')['Amount'].sum().sort_values(ascending = False).head(5)
+        st.subheader('Biggest Investments')
+        st.dataframe(big_series)
+    # fig,ax = plt.subplots()
+    # st.pyplot(big_series.plot(kind = 'bar'))
+
+####################################################
 if option == 'Overall Analysis':
     st.title('Overall Analysis')
 elif option == 'StartUp':
@@ -36,6 +57,9 @@ elif option == 'StartUp':
     st.title('Startup Analysis')
     btn1 = st.sidebar.button('Find Startup Details')
 elif option == 'Investor':
-    st.sidebar.selectbox('Select Investor',investor_data)
-    st.title('Investor Analysis')
+    selected_investor = st.sidebar.selectbox('Select Investor',investor_data)
     btn2 = st.sidebar.button('Find Investor Details')
+    
+    if btn2 :
+        load_investor_details(selected_investor)
+        
